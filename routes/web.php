2df1c0fb217ef7e4c08b3885316c829e7f4d698d
laravel <marcos.ielect@gmail.com>
2025-04-15 13:17:00 -0300

    view classes added to the web.php file to handle the static pages

--- routes/web.php
+++ routes/web.php
@@ -4,36 +4,21 @@ use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 use App\Models\Job;
 
-Route::get('/', action: function () {
-    return view('home');
-});
 
-Route::get('/about', function () {
-    return view('about');
-});
-
-Route::get('/contact', function () {
-    return view('contact');
-});
-
-
+Route::view('/', 'home');
+Route::view('/about', 'about');
+Route::view('/contact', 'contact');
 //index
 Route::get('/jobs', [JobController::class, 'index']); 
-
 // Create
 Route::get('/jobs/create', [JobController::class, 'create']);  
-
 //store
 Route::post('/jobs', [JobController::class, 'store']);
-
 //show 
 Route::get('/jobs/{job}', [JobController::class, 'show']);
-
 // Edit
 Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
-
 // Update
 Route::patch('/jobs/{job}', [JobController::class, 'update']);
-
 // destroy
 Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
