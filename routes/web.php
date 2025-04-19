71beb256e0a9c640d14883f94ba449c806cb38c0
laravel <marcos.ielect@gmail.com>
2025-04-19 16:05:07 -0300

    cambiamos todas las definiciones de rutas asociadas a Job por una unica que hace referencia a al recurso Job agregando el controlador como una forma de no tener que acceder a todas una por una

--- routes/web.php
+++ routes/web.php
@@ -8,17 +8,38 @@ use App\Models\Job;
 Route::view('/', 'home');
 Route::view('/about', 'about');
 Route::view('/contact', 'contact');
-//index
-Route::get('/jobs', [JobController::class, 'index']); 
-// Create
-Route::get('/jobs/create', [JobController::class, 'create']);  
-//store
-Route::post('/jobs', [JobController::class, 'store']);
-//show 
-Route::get('/jobs/{job}', [JobController::class, 'show']);
-// Edit
-Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
-// Update
-Route::patch('/jobs/{job}', [JobController::class, 'update']);
-// destroy
-Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
+
+Route::resource('jobs', JobController::class);
+
+// Route::controller(JobController::class)->group(function(){
+//     //index
+//     Route::get('/jobs',  'index'); 
+//     // Create
+//     Route::get('/jobs/create',  'create');  
+//     //store
+//     Route::post('/jobs',  'store');
+//     //show 
+//     Route::get('/jobs/{job}',  'show');
+//     // Edit
+//     Route::get('/jobs/{job}/edit',  'edit');
+//     // Update
+//     Route::patch('/jobs/{job}',  'update');
+//     // destroy
+//     Route::delete('/jobs/{job}',  'destroy');
+// });
+
+
+// //index
+// Route::get('/jobs', [JobController::class, 'index']); 
+// // Create
+// Route::get('/jobs/create', [JobController::class, 'create']);  
+// //store
+// Route::post('/jobs', [JobController::class, 'store']);
+// //show 
+// Route::get('/jobs/{job}', [JobController::class, 'show']);
+// // Edit
+// Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
+// // Update
+// Route::patch('/jobs/{job}', [JobController::class, 'update']);
+// // destroy
+// Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
