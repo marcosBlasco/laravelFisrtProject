deab08fcd78bfd5e1a0147d62505f8599ccd850d
laravel <marcos.ielect@gmail.com>
2025-04-20 18:15:16 -0300

    we have created some blade components so we'll be using them in the future

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -11,41 +11,29 @@
 
                 
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                    <div class="sm:col-span-4">
-                        <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
-                        <div class="mt-2">
-                            <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                            
-                            <input type="text" name="title" id="title" class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" placeholder="Programmer" required>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                @error('title')
-                    <p class="text-sm text-red-500 font-semibold mt-1">{{ $message }}</p>
-                @enderror
+                    <x-form-field>
+                        <x-form-label for="title">Title</x-form-label>
 
-
-                <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                    <div class="sm:col-span-4">
-                        <label for="salary" class="block text-sm/6 font-medium text-gray-900">Salary</label>
                         <div class="mt-2">
-                            <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                            
-                            <input type="text" name="salary" id="salary" class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" placeholder="$ 50000 per year" required>
-                            </div>
+                            <x-form-input type="text" name="title" id="title" placeholder="Programmer" required/>
+                            <x-form-error name="title"/>
                         </div>
-                    </div>
+                    </x-form-field>
+                    <x-form-field>
+                        <x-form-label for="salary">Salary</x-form-label>
+
+                        <div class="mt-2">
+                            <x-form-input type="text" name="salary" id="salary" placeholder="$50000" required/>
+                            <x-form-error name="salary"/>
+                        </div>
+                    </x-form-field>
                 </div>
-                @error('salary')
-                    <p class="text-sm text-red-500 font-semibold mt-1">{{ $message }}</p>
-                @enderror
             </div>
         </div>
 
         <div class="mt-6 flex items-center justify-end gap-x-6">
             <a href="/jobs/" class="text-sm/6 font-semibold text-gray-900">Cancel</a>
-            <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+            <x-form-button>Save</x-form-button>
         </div>
     </form>
 
